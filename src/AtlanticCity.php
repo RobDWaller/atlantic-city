e198f428874100be272a1e703bafda2bef885538
RobDWaller <rdwaller1984@googlemail.com>
2019-02-11 16:20:57 +0000

    Added getLyrics method to AtlanticCity class along with tests.

--- src/AtlanticCity.php
+++ src/AtlanticCity.php
@@ -6,5 +6,46 @@ namespace App;
 
 class AtlanticCity
 {
+    private const LYRICS = [
+        "Well they blew up the chicken man in Philly last night",
+        "Now they blew up his house too",
+        "Down on the boardwalk they're getting ready for a fight",
+        "Gonna see what them racket boys can do",
+        "Now there's trouble busting in from outta state",
+        "And the D.A. can't get no relief",
+        "Gonna be a rumble out on the promenade",
+        "And the gambling commissions hanging on by the skin of its teeth",
+        "Well now everything dies baby that's a fact",
+        "But maybe everything that dies someday comes back",
+        "Put your makeup on, fix your hair up pretty",
+        "And meet me tonight in Atlantic City",
+        "Well I got a job and tried to put my money away",
+        "But I got debts that no honest man can pay",
+        "So I drew what I had from the Central Trust",
+        "And I bought us two tickets on that Coast City bus",
+        "Now baby everything dies baby that's a fact",
+        "But maybe everything that dies someday comes back",
+        "Put your makeup on, fix your hair up pretty",
+        "And meet me tonight in Atlantic City",
+        "Now our luck may have died and our love may be cold",
+        "But with you forever I'll stay",
+        "We're going out where the sand's turning to gold",
+        "So put on your stockings, baby, 'cause the night's getting cold",
+        "And maybe everything dies, that's a fact",
+        "But maybe everything that dies someday comes back",
+        "Now I've been looking for a job but it's hard to find",
+        "Down here it's just winners and losers and don't get caught on the wrong side of that line",
+        "Well I'm tired of coming out on this losing end",
+        "So honey last night I met this guy and I'm gonna do a little favor for him",
+        "Well I guess everything dies baby that's a fact",
+        "But maybe everything that dies someday comes back",
+        "Put your hair up nice and set up pretty",
+        "And meet me tonight in Atlantic City",
+        "Meet me tonight in Atlantic City"
+    ];
 
+    public function getLyrics(): array
+    {
+        return self::LYRICS;
+    }
 }
--- tests/AtlanticCityTest.php
+++ tests/AtlanticCityTest.php
@@ -13,4 +13,24 @@ class AtlanticCityTest extends TestCase
 
         $this->assertInstanceOf(AtlanticCity::class, $atlantic);
     }
+
+    public function testGetLyrics()
+    {
+        $atlantic = new AtlanticCity();
+
+        $result = $atlantic->getLyrics();
+
+        $this->assertCount(35, $result);
+    }
+
+    public function testGetLyricsCheckLyric()
+    {
+        $atlantic = new AtlanticCity();
+
+        $result = $atlantic->getLyrics();
+
+        $this->assertSame("Well they blew up the chicken man in Philly last night", $result[0]);
+        $this->assertSame("Now baby everything dies baby that's a fact", $result[16]);
+        $this->assertSame("Meet me tonight in Atlantic City", $result[34]);
+    }
 }
