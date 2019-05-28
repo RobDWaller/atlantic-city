ae7f29079e87a7df2e0ecc764993407114d8c6ec
RobDWaller <rdwaller1984@googlemail.com>
2019-05-28 08:11:53 +0100

    Amended comments in atlantic-city file so versions are correct.

--- atlantic-city.php
+++ atlantic-city.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Atlantic City
- * @version 0.1.0-alpha
+ * @version 0.1.0
  */
 /*
 Plugin Name: Atlantic City
 Plugin URI: https://github.com/RobDWaller/atlantic-city
 Description: A Composer based WordPress plugin which displays lyrics from the song Atlantic City in the WordPress admin.
 Author: Rob Waller
-Version: 0.1.0-alpha
+Version: 0.1.0
 Author URI: http://rbrt.wllr.info
 */
 $vendor = 'vendor/autoload.php';
